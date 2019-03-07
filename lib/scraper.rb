58c020400b3180f4f5306ffcf45ede1b1ef9f049
Joan Lyness <joanrigdon@gmail.com>
2019-03-07 15:53:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -29,9 +29,9 @@ class Scraper
     array = doc.css('ul.fast_facts li').text.delete("\t").delete("\n").delete("\r").split("  ")
     # => "Acronym: AcrimSatType: OrbiterStatus: PastLaunch Date: December 20, 1999, Launch Location: Vandenberg Air Force #Base, CaliforniaMission End Date: August 08, 2014Target: EarthCurrent Location: Earth Orbit\r\nAltitude: 713 km #(apogee), #672 km (perigee)"
     #binding.pry
-    sets = array.map {|el| el.split(":")}
+    new = array.map {|el| el.split(":")}
     #binding.pry
-    sets.each do |el|
+    new.each do |el|
       k = el[0].to_sym
       v = el[1].delete(": ")
       attributes[k] = v
